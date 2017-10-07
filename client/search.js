aed6c14bd09cdf6f650393f38c8872a71f073c8c
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-07 12:51:44 -0700

    Fix typo.

--- client/search.js
+++ client/search.js
@@ -1,5 +1,8 @@
 let algoliasearch = require('algoliasearch');
 let algoliasearchHelper = require('algoliasearch-helper');
 
-module.exports.client = algoliasearch('OXB537J4TM', 'fdfc76955aeeeccf7a721bffc0878b43') //TODO: consider hiding?
-module.exports.helper = algoliasearchHelper(client, 'Restaurants')
+let client = algoliasearch('OXB537J4TM', 'fdfc76955aeeeccf7a721bffc0878b43') //TODO: consider hiding?
+let helper = algoliasearchHelper(client, 'Restaurants')
+
+module.exports.client = client;
+module.exports.helper = helper;
