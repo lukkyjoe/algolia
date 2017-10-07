3a2dc6df718d8c8c258b29a703d1d00f101dac84
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-07 12:17:06 -0700

    Test onChange.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+let log = () => {console.log('hiii')}
+
 export default class App extends React.Component {
   render() {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>hi Algolia</h1>
+        <input onChange={log}/>
       </div>
     )
   }
