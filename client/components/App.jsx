17971a72fb96148f70f726aefb9c63304bc65670
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-07 12:36:42 -0700

    Implement AlgoliaSearch locally.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+// let search = require('../search');
+
+
+let algoliasearch = require('algoliasearch');
+let algoliasearchHelper = require('algoliasearch-helper');
+
+let client = algoliasearch('OXB537J4TM', 'fdfc76955aeeeccf7a721bffc0878b43') //TODO: consider hiding?
+let helper = algoliasearchHelper(client, 'Restaurants')
 
 let log = () => {console.log('hiii')}
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.foo = this.foo.bind(this);
+  }
+  
+  foo(){
+    console.log('siup')
+    helper.on('result', (content) => {console.log(content)})
+    helper.search()
+  }
+
   render() {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>hi Algolia</h1>
-        <input onChange={log}/>
+        <input onChange={this.foo}/>
       </div>
     )
   }
