5076b09a7938d7f5289315596354042d868e3987
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-06 22:41:30 -0700

    Relocate index.html.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
-  inject: 'body',
+  inject: 'body'
 })
 
 module.exports = {
@@ -13,6 +13,7 @@ module.exports = {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
   },
+  devtool: 'source-map',
   module: {
     loaders: [
         { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
