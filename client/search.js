9abce88598e5f3d3cced9ac8e05a348a3c067616
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-08 10:54:30 -0700

    Add food_type facet after adding attributes for faceting on dashboard.

--- client/search.js
+++ client/search.js
@@ -2,7 +2,9 @@ let algoliasearch = require('algoliasearch');
 let algoliasearchHelper = require('algoliasearch-helper');
 
 let client = algoliasearch('OXB537J4TM', 'fdfc76955aeeeccf7a721bffc0878b43') //TODO: consider hiding?
-let helper = algoliasearchHelper(client, 'Restaurants')
+let helper = algoliasearchHelper(client, 'Restaurants', {
+  facets: ['food_type']
+})
 
 module.exports.client = client;
 module.exports.helper = helper;
