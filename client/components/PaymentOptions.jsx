839e4f2674192e5129cdb8574d3ec13cca6796b2
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-09 07:40:46 -0700

    Add checkbox.

--- client/components/PaymentOptions.jsx
+++ client/components/PaymentOptions.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const PaymentOptions = (props) => {
   const options = props.options;
-  let list = options.map((item, index) => <div key={index} onClick={() => {props.select(item)
-    console.log('selected', item)
-    }}> {item}</div>)
+  let list = options.map((item, index) => {
+      return (
+        <div key={index} onClick={() => {props.select(item)
+            console.log('selected', item)
+            }}>
+          <div> {item}</div>
+          <input type="checkbox"/>
+        </div>
+      )
+    })
   return <div>{list}</div>
 }
 
