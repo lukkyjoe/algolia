b62331c03f81637fe5db30dbaa07e9222faa554d
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-10 10:23:21 -0700

    InitIndex.

--- client/search.js
+++ client/search.js
@@ -6,6 +6,9 @@ let helper = algoliasearchHelper(client, 'Restaurants', {
   facets: ['food_type', 'stars_count', 'payment_options'], //TODO: check if still necessary
   disjunctiveFacets: ['payment_options']
 })
+let index = client.initIndex('Restaurants');
+
 
 module.exports.client = client;
 module.exports.helper = helper;
+module.exports.index = index;
