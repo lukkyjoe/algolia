a974d34fb02e1ad86e411d8dae7416fcad7b17df
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-10 13:38:53 -0700

    Add image thumbnails.

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Results = (props) => {
   const rawResults = props.rawResults;
-  let list = rawResults.map((item, index) => <div key={index}> {item.name} : {item.food_type} : {item.stars_count} : {item.neighborhood} : {item.payment_options} </div>)
+  let list = rawResults.map((item, index) => <div key={index}> <img src={item.image_url}></img>{item.name} : {item.food_type} : {item.stars_count} : {item.neighborhood} : {item.payment_options} </div>)
+  
+  
+  
   return <div>{list}</div>
 }
 
