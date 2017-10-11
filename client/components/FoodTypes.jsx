71b42dcbad06da7422a01582e48dc14d586f2622
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-10 22:21:43 -0700

    Implement hover state.

--- client/components/FoodTypes.jsx
+++ client/components/FoodTypes.jsx
@@ -3,7 +3,7 @@ import styles from './FoodTypes.css'
 
 const FoodTypes = (props) => {
   const foodTypes = props.food_types;
-  let list = foodTypes.map((item, index) => <div className={styles.food} key={index} onClick={() => {props.select(item.name)}}> {item.name}</div>)
+  let list = foodTypes.map((item, index) => <div className={styles.foodFilter} key={index} onClick={() => {props.select(item.name)}}> {item.name}</div>)
   return <div><h3>Cuisine/Food Type</h3>{list}</div>
 }
 
