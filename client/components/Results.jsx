97573c5cb5aca1f664f796856e066119d2864a6e
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-11 05:47:20 -0700

    Render stars_count.

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -7,6 +7,31 @@ const Results = (props) => {
 }
 
 const SingleResult = (props) => {
+  let roundDownToHalf = (num) => {
+    return Math.floor(num*2)/2;
+  }
+  let renderStarsScore = () => {
+    let starsArray = [];
+    let targetScore = roundDownToHalf(props.item.stars_count);
+    console.log('targetScore', targetScore)
+    let fullStars = Math.floor(targetScore);
+    console.log('fullStars', fullStars)
+    let halfStar = (targetScore % 1) > 0 ? 1 : 0 
+    console.log('halfStar', halfStar)
+    let emptyStars = 5 - fullStars - halfStar;
+    console.log('emptyStars', emptyStars)
+    for (let i = 0; i < fullStars; i++){
+      starsArray.push(<img style={{height: '20px', width: '20px'}} src="https://s3-us-west-1.amazonaws.com/algolia-graphics/plainStars.png"></img>)
+    }
+    if (halfStar) {
+      starsArray.push(<img style={{height: '20px', width: '20px'}} src="https://s3-us-west-1.amazonaws.com/algolia-graphics/halfstar-whole.png"></img>)
+    }
+    for (let i = 0; i < emptyStars; i++){
+      starsArray.push(<img style={{height: '20px', width: '20px'}} src="https://s3-us-west-1.amazonaws.com/algolia-graphics/star-empty.png"></img>)
+    }
+    console.log(starsArray);
+    return starsArray;
+  }
   return(
     <div style={{display: 'flex'}}>
       <div><img src={props.item.image_url} style={{width: '80px', height: '80px'}}></img></div> 
@@ -15,6 +40,7 @@ const SingleResult = (props) => {
         <div style={{display: 'flex'}}>
           <span>{props.item.stars_count}</span>
           <span>({props.item.reviews_count})</span>
+          {renderStarsScore()}
         </div>
         <div style={{display: 'flex'}}>
           <span>{props.item.food_type} | </span>
