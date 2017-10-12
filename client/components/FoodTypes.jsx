7d019864821aa8f7071718a7c50efa2f30d8d7dc
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-11 17:28:15 -0700

    Refactor SingleFoodType to component.

--- client/components/FoodTypes.jsx
+++ client/components/FoodTypes.jsx
@@ -3,8 +3,16 @@ import styles from './App.css'
 
 const FoodTypes = (props) => {
   const foodTypes = props.food_types;
-  let list = foodTypes.map((item, index) => <div className={styles.filter} key={index} onClick={() => {props.select(item.name)}}> <div>{item.name}</div> <div className={styles.facetCount}>{item.count}</div></div>)
+  let list = foodTypes.map((item, index) => <SingleFoodType item={item} key={index} select={props.select}/>)
   return <div><h3>Cuisine/Food Type</h3>{list}</div>
+}
+
+const SingleFoodType = (props) => {
+  return (
+    <div className={styles.filter} onClick={() => {props.select(props.item.name)}}> 
+      <div>{props.item.name}</div> <div className={styles.facetCount}>{props.item.count}</div>
+    </div>
+  )
 }
 
 export default FoodTypes;
